d8a6fd77fe5786505a8194cc69ec676972d501e5
Joe <joe.han0212@gmail.com>
2017-09-11 15:16:59 -0500

    update commander view

--- src/components/LosingCommander.vue
+++ src/components/LosingCommander.vue
@@ -1,16 +1,17 @@
 <template>
   <v-container id="losing-commander">
-  <h4>Col Thorson, Commander of 35 OG</h4>
+  <h4>Welcome Col Thorson, Commander of 99 OG</h4>
   <v-layout row wrap>
   <v-flex xs12>
   <h6>FGO</h6>
   <v-expansion-panel>
     <v-expansion-panel-content v-for="(officer,i) in FGO" :key="i">
-      <div slot="header">{{officer.grade}} {{officer.name}} // Rank : {{ officer.rank }} // Quals: {{officer.qualifications.join(', ')}}</div>
+      <div slot="header">{{officer.grade}} {{officer.name}} // Rank : {{ officer.rank }}</div>
       <v-card>
         <v-card-text class="grey lighten-3">
-        <div>Rank and Qualify</div>
-        <v-layout row wrap>
+        <div>Rank and Qualify
+          <v-layout row>
+          <v-flex xs4>
           <v-select
             :items="fgoRanks"
             v-model="officer.rank"
@@ -18,18 +19,24 @@
             single-line
             bottom
           ></v-select>
+          </v-flex>
           <v-spacer></v-spacer>
+          <v-flex xs4>
           <v-select
-            :items="qualList"
+            :items="qualOptions(officer.qualifications)"
             v-model="officer.qualifications"
             label="Qualifications"
             multiple
             single-line
             bottom
           ></v-select>
-        </v-layout>
+          {{officer.qualifications}}
+          </v-flex>
+          </v-layout>
+        </div>
         <div v-for="(vector, i) in officer.vectors">Vector {{i+1}}
-          <v-layout row justify-space-around>
+          <v-layout row>
+            <v-flex xs3>
             <v-select
               class='mr-3'
               :items="vectorList"
@@ -38,112 +45,28 @@
               single-line
               bottom
             ></v-select>
+            </v-flex>
+            <v-flex xs2 v-for="i in vectorLengthLocation(vector.vector)" :key="i">
             <v-select
               class='mr-3'
               :items="vectorLocation[vector.vector]"
-              v-model="vector.location[0]"
+              v-model="vector.location[i]"
               label="Location"
               single-line
               bottom
             ></v-select>
+            </v-flex>
           </v-layout>
         </div>
         <div>Supporting Information</div>
-        <v-layout row justify-space-around>
-        <v-flex xs-4>  
+        <v-layout row>
+        <v-flex xs4>  
             <v-checkbox color="primary" label="Join Spouse" v-model="officer.joinSpouse"></v-checkbox>
         </v-flex>
-        <v-flex xs-4>
+        <v-flex xs4>
             <v-checkbox color="primary" label="EFMP/Humanitarian" v-model="officer.EFMP"></v-checkbox>
         </v-flex>
-        <v-flex xs-4>
-            <v-checkbox color="primary" label="RNLTD Issue?" v-model="officer.issueRNLTD"></v-checkbox>
-        </v-flex>
-        </v-layout>
-          <v-text-field
-                name="og-cc comment"
-                label="OG/CC Comment"
-                v-model="officer.comment_commander"
-                textarea
-          ></v-text-field>
-        </v-card-text>
-      </v-card>
-    </v-expansion-panel-content>
-  </v-expansion-panel>
-  </v-flex>
-  </v-layout>
-  <v-layout row wrap class="pt-3">
-  <v-flex xs12>
-  <h6>CGO</h6>
-  <v-expansion-panel>
-    <v-expansion-panel-content v-for="(officer,i) in CGO" :key="i">
-      <div slot="header">{{officer.grade}} {{officer.name}} // Rank : {{ officer.rank }} // Quals: {{officer.qualifications.join(', ')}}</div>
-      <v-card>
-        <v-card-text class="grey lighten-3">
-        <div>Rank and Qualify</div>
-        <v-layout row wrap>
-          <v-select
-            :items="fgoRanks"
-            v-model="officer.rank"
-            label="Rank"
-            single-line
-            bottom
-          ></v-select>
-          <v-spacer></v-spacer>
-          <v-select
-            :items="qualList"
-            v-model="officer.qualifications"
-            label="Qualifications"
-            multiple
-            single-line
-            bottom
-          ></v-select>
-        </v-layout>
-        <div v-for="(vector, i) in officer.vectors">Vector {{i+1}}
-          <v-layout row justify-space-around>
-            <v-select
-              class='mr-3'
-              :items="vectorList"
-              v-model="vector.vector"
-              label="Vector"
-              single-line
-              bottom
-            ></v-select>
-            <v-select
-              class='mr-3'
-              :items="vectorLocation[vector.vector]"
-              v-model="vector.location1"
-              label="1st Location"
-              single-line
-              bottom
-            ></v-select>
-            <v-select
-              class='mr-3'
-              :items="vectorLocation[vector.vector]"
-              v-model="vector.location2"
-              label="2nd Location"
-              single-line
-              bottom
-            ></v-select>
-            <v-select
-              class='mr-3'
-              :items="vectorLocation[vector.vector]"
-              v-model="vector.location3"
-              label="3rd Location"
-              single-line
-              bottom
-            ></v-select>
-          </v-layout>
-        </div>
-        <div>Supporting Information</div>
-        <v-layout row justify-space-around>
-        <v-flex xs-4>  
-            <v-checkbox color="primary" label="Join Spouse" v-model="officer.joinSpouse"></v-checkbox>
-        </v-flex>
-        <v-flex xs-4>
-            <v-checkbox color="primary" label="EFMP/Humanitarian" v-model="officer.EFMP"></v-checkbox>
-        </v-flex>
-        <v-flex xs-4>
+        <v-flex xs4>
             <v-checkbox color="primary" label="RNLTD Issue?" v-model="officer.issueRNLTD"></v-checkbox>
         </v-flex>
         </v-layout>
@@ -164,7 +87,7 @@
     :timeout="3000"
     :top="true"
     v-model="snackbar"
-  >Submitted!<v-btn flat class="pink--text" @click.native="snackbar = false">Close</v-btn></v-snackbar>
+  >Submitted!<v-btn flat class="blue--text" @click.native="snackbar = false">Close</v-btn></v-snackbar>
 </v-container>
 
 </template>
@@ -180,38 +103,83 @@ export default{
             vectorList:[
               'None',
               'Slip',
-              'Remote',
-              'Test',
-              'F-35',
+              'A-10',
+              'F-15C',
+              'F-15E',
               'F-16 Ops',
               'F-16 FTU',
-              'IFF/T-38',
-              'Active Associate'
+              'F-22 Ops',
+              'F-35',
+              'IFF',
+              'UPT: T-6/T-38',
+              'White Jet',
+              'Aggressors',
+              'Test',
+              'Staff',
+              'TFI',
+              'ALO'
             ],
             vectorLocation:{
-              'None': ['None'],
-              'Slip': ['Current Location'],
-              'Remote': ['Kunsan', 'Osan'],
-              'Test': ['Edwards', 'Eglin','Nellis'],
-              'F-35': ['Eglin', 'Luke', 'Hill'],
-              'F-16 Ops': ['Aviano', 'Misawa', 'Shaw', 'Spangdahlem'],
+              'A-10': ['Davis-Monthan','Osan', 'Moody','TFI-Whiteman'],
+              'F-15C': ['Kadena', 'Lakenheath','Klamath Falls FTU','Barnes TFI', 'Jacksonville TFI', 'New Orleans TFI', 'Portland TFI','Fresno TFI'],
+              'F-15E': ['Mountain Home', 'Lakenheath', 'Seymour Johnson Ops', 'Seymour Johnson FTU'],
+              'F-16 Ops': ['Aviano', 'Kunsan', 'Osan', 'Misawa', 'Shaw', 'Spangdahlem'],
               'F-16 FTU': ['Holloman', 'Luke'],
-              'IFF/T-38': ['Columbus', 'Randolph', 'Sheppard'],
-              'AGRS': ['Nellis', 'Eielson'],
-              'Active Associate': ['Some guard base']
+              'F-22 Ops': ['Elmendorf', 'Langley', 'Tyndall', 'TFI-Hickham'],
+              'F-35': ['Eglin', 'Luke', 'Hill'],
+              'IFF': ['Columbus', 'Randolph', 'Sheppard'],
+              'UPT: T-6/T-38': ['Columbus', 'Laughlin', 'Randolph', 'Sheppard', 'Vance'],
+              'Aggressors': ['Nellis', 'Eielson'],
+              'Test': ['Edwards', 'Eglin','Nellis','Other'],
+              'Staff': ['OSD','SAF', 'HAF', 'MAJCOM', 'NAF', 'Joint' ],
+              'TFI': ['Baltimore', 'Forth Worth'],
             },
-            qualList:[
-              'SEFE',
-              'WIC',
-              'IP',
-              '4-ship',  
-              '2-ship',
-              'Wingman'
-            ],
             snackbar: false,
             cgoRanks: [],
             fgoRanks: [],
         }
+    },
+    methods: {
+      vectorLengthLocation(vector){
+        if (this.vectorLocation[vector]){
+          //Only give three options for losing OGs for locations within a vector
+          return Math.min(this.vectorLocation[vector].length,3)
+        }
+        else {
+          // If the vector doesn't exist yet, don't render location boxes
+          return 0
+        }
+      },
+      qualOptions(quals){
+        console.log(quals)
+        var qualList =[
+              'WIC',
+              'SEFE',
+              'MSN/CC',
+              'IP',
+              '4-ship',  
+              '2-ship',
+              'Wingman',
+              'Safety',
+              'FAC(A)',
+              'SANDY',
+        ]
+        if (quals.length == 0){
+          return qualList
+        }
+        else if (quals.indexOf('WIC')>=0){
+          return ['WIC','SEFE', 'Safety', 'FAC(A)', 'SANDY']
+        }
+        else if (quals.indexOf('SEFE')>=0){
+          return ['WIC','SEFE','MSN/CC','Safety', 'FAC(A)', 'SANDY']
+        }
+        else if (quals.indexOf('IP')>=0){
+          return ['WIC','SEFE','MSN/CC','IP','Safety', 'FAC(A)', 'SANDY']
+        }
+        else {
+          return qualList
+        }
+      }
     },
     created: function(){
       var totalCGO = this.CGO.length
